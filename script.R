78c8c267859f875d54151e79813b331efea531db
marihebnes <mari.tobekk@gmail.com>
2022-09-27 11:29:10 +0200

    kk

--- script.R
+++ script.R
@@ -1,3 +1,9 @@
-# Tests
-
-# New commit
+data <- read_xlsx("data/humac-data.xlsx") %>%
+  select(fp, timepoint, leg, pt.60) %>%
+  pivot_wider(names_from = timepoint, values_from = pt.60) %>%
+  mutate(diff = pre - post) %>%
+  summarise(s = sd(diff),
+            te = s / sqrt(2),
+            m = mean(c(pre, post)),
+            cv = (te / m) * 100) %>%
+  print()
